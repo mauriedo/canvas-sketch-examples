a0ad4b3f1785e7f9597140c9db73312508b541fc
Mauri Edo <medo@atlassian.com>
2021-11-14 09:58:48 +1100

    initialise coordinates and cookies only once

--- fortune-cookie/fortune-cookie.js
+++ fortune-cookie/fortune-cookie.js
@@ -3,41 +3,42 @@ import canvasSketch from 'canvas-sketch';
 import {Cookie, Point} from './objects';
 import {prepareCanvas} from './utils';
 
-const radius = 96;
-const width = radius * 16;
-const height = width / 2;
+const RADIUS = 96;
+const WIDTH = RADIUS * 16;
+const HEIGHT = WIDTH / 2;
 
 const settings = {
-  dimensions: [ width, height ],
+  dimensions: [ WIDTH, HEIGHT ],
   animate: true,
   playbackRate: 'throttle',
   fps: 0.5
 };
 
-const sketch = () => {
+const sketch = ({ width, height }) => {
+  const cookies = [];
+
+  const coordinates = [
+    // First row
+    new Point(1 * width / 8, height / 4),
+    new Point(3 * width / 8, height / 4),
+    new Point(5 * width / 8, height / 4),
+    new Point(7 * width / 8, height / 4),
+    // Second row
+    new Point(1 * width / 8, 3 * height / 4),
+    new Point(3 * width / 8, 3 * height / 4),
+    new Point(5 * width / 8, 3 * height / 4),
+    new Point(7 * width / 8, 3 * height / 4),
+  ];
+
+  coordinates.forEach((coordinate) => cookies.push(new Cookie(coordinate)));
+
   return ({ context, width, height }) => {
 
     prepareCanvas(context, width, height);
 
-    const cookies = [];
-
-    const coordinates = [
-        // First row
-        new Point(1 * width / 8, height / 4),
-        new Point(3 * width / 8, height / 4),
-        new Point(5 * width / 8, height / 4),
-        new Point(7 * width / 8, height / 4),
-        // Second row
-        new Point(1 * width / 8, 3 * height / 4),
-        new Point(3 * width / 8, 3 * height / 4),
-        new Point(5 * width / 8, 3 * height / 4),
-        new Point(7 * width / 8, 3 * height / 4),
-    ];
-    coordinates.forEach((coordinate) => cookies.push(new Cookie(coordinate)));
-
     cookies.forEach((cookie) => {
-      cookie.prepare(context, radius);
-      cookie.bite(context, radius);
+      cookie.prepare(context, RADIUS);
+      cookie.bite(context, RADIUS);
     });
   };
 };
